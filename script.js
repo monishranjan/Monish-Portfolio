14bd5a3964024c65abe0d4d6c60395b878ef7233
Monish Ranjan <monishranjan9@gmail.com>
2024-05-20 15:18:32 +0530

    Adding loader pages

--- script.js
+++ script.js
@@ -4,4 +4,28 @@ function sendEMail() {
 }
 // ====================================
 
+// Custom Loader
+var tl = gsap.timeline()
+
+tl.from("#custom_loader h1", {
+    x:40,
+    opacity:0,
+    duration:1,
+    stagger:0.1,
+})
+
+tl.to("#custom_loader h1", {
+    opacity:0,
+    x:-40,
+    duration:1,
+    stagger:0.1,
+})
+tl.to("#custom_loader", {
+    opacity:0,
+})
+t.to("#custom_loader", {
+    display:"none"
+})
+// ====================================
+
 // Starting the Functions
